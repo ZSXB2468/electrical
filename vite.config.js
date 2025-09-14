339240fc61ff097e3977ded973ed6e9b3ecb79e0
ZSXB2468 <zsxb-2468@qq.com>
2025-09-14 14:49:40 +0800

    style: 格式化vite.config.js文件

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-  root: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html'  // 以 index.html 作为入口点，而不是 index.ts
+    root: './',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: './index.html'
+        }
+    },
+    server: {
+        port: 10890,
+        open: true
     }
-  },
-  server: {
-    port: 10890,
-    open: true
-  }
 });
